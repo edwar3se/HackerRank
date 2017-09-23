7c314ab1251fc39b17f9262e925734ba0a03c68c
Sydney Edwards <edwar3se@dukes.jmu.edu>
2017-09-23 19:11:42 -0400

    Create IsThisABinarySearchTree.java

---
+++ IsThisABinarySearchTree.java
@@ -0,0 +1,16 @@
+/* Hidden stub code will pass a root argument to the function below. Complete the function to solve the challenge. Hint: you may want to write one or more helper functions.  
+
+The Node class is defined as follows:
+    class Node {
+        int data;
+        Node left;
+        Node right;
+     }
+     
+     FALSE if: left node is equal or greater than root, right node is less than or equal to root
+      - everything on left must be less than the original root
+*/
+    boolean checkBST(Node root) {
+        
+}
+
