6814e0147dbb2a6922e3269029eb1bf8721ad251
Sydney Edwards <edwar3se@dukes.jmu.edu>
2017-09-24 14:44:02 -0400

    Update IsThisABinarySearchTree.java

--- IsThisABinarySearchTree.java
+++ IsThisABinarySearchTree.java
@@ -7,10 +7,55 @@ The Node class is defined as follows:
         Node right;
      }
      
-     FALSE if: left node is equal or greater than root, right node is less than or equal to root
-      - everything on left must be less than the original root
+     FALSE if:  left node is equal or greater than root, 
+                anything on left side of tree is greater than original root,
+                right node is less than or equal to root,
+                anything on ride side of tree is greater than original root
 */
-    boolean checkBST(Node root) {
+
+    boolean checkBST(Node root) 
+    {
+        if (root == null)
+            return true;
         
-}
+        return left(root.left, root.data) && right(root.right, root.data);
+    }
+
+    boolean left(Node root, int original)
+    {
+        
+        if(root.data >= original)
+            return false;
+        
+        if(root.left != null && root.right != null)
+        {
+            if(root.left.data >= root.data || root.right.data <= root.data)
+                return false;
+            
+            return left(root.left, original) && left(root.right, original);
+        }
+        
+        return true;
+    }
+
+    boolean right(Node root, int original)
+    {
+        
+        if(root.data <= original)
+            return false;
+        
+        if((root.left == null && root.right != null) || (root.left != null && root.right == null))
+            return false;
+        
+        if(root.left != null && root.right != null)
+        {
+            
+            if(root.left.data >= root.data || root.right.data <= root.data)
+                return false;
+            
+            return right(root.left, original) && right(root.right, original);
+        }
+        
+        return true;
+    }
 
